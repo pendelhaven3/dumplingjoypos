7b261f1342713f621e81d5513c51fabccda5d63e
PJ Miranda <pendelhaven3@gmail.com>
2012-06-25 23:15:37 +0800

    Add fields to Customer and Supplier domains

--- grails-app/domain/com/dumplingjoy/pos/Customer.groovy
+++ grails-app/domain/com/dumplingjoy/pos/Customer.groovy
@@ -2,13 +2,32 @@ package com.dumplingjoy.pos
 
 class Customer {
 
-	String name
+	String code
+	String name // TODO: Change to businessname
+	String contactPerson
+	String contactNumber
+	String faxNumber
 	String address
-//	PricingScheme defaultPricingScheme // To implement later
+	String modeOfPayment
+	PaymentTerms terms
+	String bank
+	String branch
+	BigDecimal creditLine
+	String remarks
 	
     static constraints = {
+		code nullable: true // TODO: make non-nullable later
 		name blank: false, unique: true
+		contactPerson nullable: true
+		contactNumber nullable: true
+		faxNumber nullable: true
 		address blank: false
+		modeOfPayment nullable: true, inList: ["COD", "Check"]
+		terms nullable: true
+		bank nullable: true
+		branch nullable: true
+		creditLine nullable: true, min: BigDecimal.ZERO
+		remarks nullable: true
     }
 	
 }
--- grails-app/domain/com/dumplingjoy/pos/Supplier.groovy
+++ grails-app/domain/com/dumplingjoy/pos/Supplier.groovy
@@ -5,6 +5,11 @@ class Supplier {
 	String name
 	String address
 	String contactNumber
+	String contactPerson
+	String faxNumber
+	String emailAddress
+	String tin
+	PaymentTerms terms
 	
 	List<Product> products
 	
@@ -12,6 +17,11 @@ class Supplier {
 		name blank: false, unique: true
 		address blank: false
 		contactNumber blank: false
+		contactPerson nullable: true
+		faxNumber nullable: true
+		emailAddress nullable: true
+		tin nullable: true
+		terms nullable: true
     }
 
 	static hasMany = [products: Product]
