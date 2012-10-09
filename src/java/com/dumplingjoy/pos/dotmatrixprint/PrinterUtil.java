5290569a8ea76d705794a68d05f34fde4c376395
PJ Miranda <pendelhaven3@gmail.com>
2012-10-10 00:11:26 +0800

    Hardcoded application to print only on EPSON LX-300 printer

--- src/java/com/dumplingjoy/pos/dotmatrixprint/PrinterUtil.java
+++ src/java/com/dumplingjoy/pos/dotmatrixprint/PrinterUtil.java
@@ -15,25 +15,37 @@ public class PrinterUtil {
 	private static DocFlavor DOC_FLAVOR = new DocFlavor("application/octet-stream", "java.io.InputStream");
 	private static String CARRIAGE_RETURN = "\r";
 	private static String FORM_FEED = "\f";
+	private static String EPSON_PRINTER = "EPSON LX-300+ /II";
 	
 	public static void print(String data) throws PrintException {
 		print(data.getBytes());
 	}
 	
 	public static void print(byte[] data) throws PrintException {
-		PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+		PrintService[] printServices = PrintServiceLookup.lookupPrintServices(DOC_FLAVOR, null);
+		PrintService epsonPrintService = null;
+		for (PrintService printService : printServices) {
+			if (EPSON_PRINTER.equals(printService.getName())) {
+				epsonPrintService = printService;
+				break;
+			}
+		}
 		
-		DocPrintJob printJob = printService.createPrintJob();
+		if (epsonPrintService == null) {
+			throw new RuntimeException(EPSON_PRINTER + "  printer not connected");
+		}
+		
+		DocPrintJob printJob = epsonPrintService.createPrintJob();
 		Doc doc = new SimpleDoc(new ByteArrayInputStream(CARRIAGE_RETURN.getBytes()), DOC_FLAVOR, null);
 		printJob.print(doc, null);
 		
-		printJob = printService.createPrintJob();
+		printJob = epsonPrintService.createPrintJob();
 		doc = new SimpleDoc(new ByteArrayInputStream(data), DOC_FLAVOR, null);
 		printJob.print(doc, null);
 		
-		printJob = PrintServiceLookup.lookupDefaultPrintService().createPrintJob();
+		printJob = epsonPrintService.createPrintJob();
 		doc = new SimpleDoc(new ByteArrayInputStream(FORM_FEED.getBytes()), DOC_FLAVOR, null);
 		printJob.print(doc, null);
 	}
-
+	
 }
